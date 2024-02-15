a67e437c0f5bc15a8ada9b6785fbaed6846c644c
karkiyubin <therealyubin@gmail.com>
2024-02-15 15:18:44 +0545

    feat: login

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is login feature.
         </p>
       </div>
       <p className="read-the-docs">
